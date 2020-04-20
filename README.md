7d453c2c0f2e377c3aaf6a530ef851199abd73af
Satur <tiagosatur@gmail.com>
2020-04-20 10:53:26 -0300

    FEATURE | It solves reverse string algo

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# AlgoCasts
+# Algorithms Bootcamp
 
-Companion repo to a course on Udemy.com
+This repo has algorithm challenges used in coding interviews.
+
+## Setup
+
+`yarn global add jest`
+
+## Watch a specific file
+
+`cd exercides`
+
+`jest fib/test.js --watch`
+
+or
+
+`jest fib --watch`
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((reversed, char) => char + reversed, "");
+}
 
 module.exports = reverse;
+
+/* 
+// Solution 01
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+*/
+
+/* 
+// Solution 02
+function reverse(str) {
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+*/
