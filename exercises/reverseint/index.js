3925aedd6067a5a26aea5c108fee24167a498357
Satur <tiagosatur@gmail.com>
2020-04-21 13:19:49 -0300

    FEATURE | It solves the reverseInt algo

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  //To split, you must convert number to string
+  const reversedInt = Math.abs(n).toString().split("").reverse().join("");
+  return Math.sign(n) * reversedInt;
+}
+
+/* 
+// Alternative solution
+function reverseInt(n) {
+  const reversedInt = n.toString().split("").reverse().join("");
+  const parsed = parseInt(reversedInt);
+  return n < 0 ? parsed * -1 : parsed;
+}
+ */
 
 module.exports = reverseInt;
